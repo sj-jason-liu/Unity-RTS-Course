10b79794207f05321758585b4730d5bfde39ed76
sj-jason-liu <sj.jason.liu@gmail.com>
2025-07-30 00:01:44 +0800

    Added camera rotation function: Fixed camera zoom key from End to Delete. Added camera rotation function with pageUp and pageDown.

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -8,11 +8,14 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private Transform _cameraTarget;
     [SerializeField] private float _keyboardPanSpeed = 5f;
     [SerializeField] private float _zoomSpeed = 1f;
+    [SerializeField] private float _rotateSpeed = 1f;
     [SerializeField] private float minZoomDistance = 7.5f;
 
     private CinemachineFollow cinemachineFollow;
     private float zoomStartTime;
+    private float rotateStartTime;
     private Vector3 startingFollowOffset;
+    private float maxRotationAmount;
 
     private void Awake()
     {
@@ -22,12 +25,67 @@ public class PlayerInput : MonoBehaviour
             Debug.LogError("CinemachineFollow component not found on " + _cinemachineCamera.name);
         }
         startingFollowOffset = cinemachineFollow.FollowOffset;
+        maxRotationAmount = Mathf.Abs(startingFollowOffset.z);
     }
 
     void Update()
     {
         HandlePanning();
         HandleZooming();
+        HandleRotation();
+    }
+
+    private void HandleRotation()
+    {
+        //check if the rotation keys are pressed to set the start time
+        if (ShouldSetRotateStartTime())
+        {
+            rotateStartTime = Time.time;
+        }
+
+        //calculate the rotation time
+        float rotationTime = Mathf.Clamp01((Time.time - rotateStartTime) * _rotateSpeed);
+        Vector3 targetFollowOffset;  //store the target follow offset based on rotation
+
+        if (Keyboard.current.pageUpKey.isPressed && !Keyboard.current.pageDownKey.isPressed) //if pressing pageUp, camera rotates right
+        {
+            targetFollowOffset = new Vector3(
+                maxRotationAmount,
+                cinemachineFollow.FollowOffset.y,
+                0
+            );
+        }
+        else if (Keyboard.current.pageDownKey.isPressed && !Keyboard.current.pageUpKey.isPressed)  //if pressing pageDown, camera rotates left
+        {
+            targetFollowOffset = new Vector3(
+                -maxRotationAmount,
+                cinemachineFollow.FollowOffset.y,
+                0
+            );
+        }
+        else
+        {
+            targetFollowOffset = new Vector3(
+                startingFollowOffset.x,
+                cinemachineFollow.FollowOffset.y,
+                startingFollowOffset.z
+            );
+        }
+
+        //smoothly interpolate the follow offset towards the target offset
+        cinemachineFollow.FollowOffset = Vector3.Slerp(
+                cinemachineFollow.FollowOffset,
+                targetFollowOffset,
+                rotationTime
+            );
+    }
+
+    private bool ShouldSetRotateStartTime()
+    {
+        return Keyboard.current.pageUpKey.wasPressedThisFrame
+            || Keyboard.current.pageDownKey.wasReleasedThisFrame
+            || Keyboard.current.pageUpKey.wasReleasedThisFrame
+            || Keyboard.current.pageDownKey.wasPressedThisFrame;
     }
 
     private void HandleZooming()
@@ -41,7 +99,7 @@ public class PlayerInput : MonoBehaviour
         float zoomTime = Mathf.Clamp01((Time.time - zoomStartTime) * _zoomSpeed);
         Vector3 targetFollowOffset;  //store the target follow offset based on zoom in or out
 
-        if (Keyboard.current.endKey.isPressed) //if zooming in, target offset is set to minimum distance
+        if (Keyboard.current.deleteKey.isPressed) //if zooming in, target offset is set to minimum distance
         {
             targetFollowOffset = new(
                 cinemachineFollow.FollowOffset.x,
@@ -68,8 +126,8 @@ public class PlayerInput : MonoBehaviour
 
     private bool ShouldSetZoomeStartTime()
     {
-        return Keyboard.current.endKey.wasPressedThisFrame
-            || Keyboard.current.endKey.wasReleasedThisFrame;
+        return Keyboard.current.deleteKey.wasPressedThisFrame
+            || Keyboard.current.deleteKey.wasReleasedThisFrame;
     }
 
     private void HandlePanning()
